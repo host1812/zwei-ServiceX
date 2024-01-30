c164aed908187891ccc6aa062dee8f4322a4fec8
afedorenchik <afedorenchik@microsoft.com>
2024-01-30 12:24:03 -0500

    some project here

---
+++ ServiceX.Api/ServiceX.Api/BuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="BuilderExtensions.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+
+namespace ServiceX.Api.Extensions;
+
+public static class BuilderExtensions
+{
+    public static ILogger<T> CreateSimpleLogger<T>(this WebApplicationBuilder builder)
+    {
+        using var loggerFactory = LoggerFactory.Create(config =>
+        {
+            config.AddConsole()
+            .AddDebug()
+            .AddAzureWebAppDiagnostics();
+        });
+
+        return loggerFactory.CreateLogger<T>();
+    }
+}
--- ServiceX.Api/ServiceX.Api/Program.cs
+++ ServiceX.Api/ServiceX.Api/Program.cs
@@ -1,11 +1,23 @@
+using ServiceX.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Logging.AddConsole();
+
+var logger = builder.CreateSimpleLogger<Program>();
+logger.LogInformation("Example log message 4");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var aadScopes = builder.Configuration["AzureAd:Scopes"]?.Split(",")!;
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+    .EnableTokenAcquisitionToCallDownstreamApi(aadScopes)
+    .AddInMemoryTokenCaches();
 
 var app = builder.Build();
 
